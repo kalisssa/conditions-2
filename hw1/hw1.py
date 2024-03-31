03ec56897eaf8aab15e90cb6ecfebcd27e1fde7b
klsss150367 <dilatan8@ukr.net>
2024-03-31 23:54:11 +0300

    homework1

---
+++ hw1/hw1.py
@@ -0,0 +1,57 @@
+print("Hello World")
+print()
+#print("test")
+#print("qwerty")
+
+'''
+dyiuugufydtdyt7yuiufyytg
+fydtgihujht
+tffygggugu
+ddtfytuyituy8
+ftfoyouup[u
+'''
+print("Hello\n\tWorld")
+#print("hello\n\tworld")
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+#print("Name: ", name, " Age: ", age)
+print("Name: " + name + " Age: " + str(age))
+print(age + 20)
+
+
+#homework
+#1
+number = int(input("Enter 3-digital number: "))
+n1 = number // 100
+n2 = number // 10 % 10
+n3 = number % 10
+result = n1 + n2 + n3
+print(f"n1: {n1} n2: {n2} n3 {n3}")
+print(f"result: {result}")
+
+number = int(input("Enter 3-digital number: "))
+n1 = number // 100
+n2 = number // 10 % 10
+n3 = number % 10
+result = n1 * n2 * n3
+print(f"n1: {n1} n2: {n2} n3: {n3}")
+print(f"result: {result}")
+
+#2
+
+diagonal1 = int(input(f"Enter diagonal: "))
+diagonal2 = int(input(f"Enter diagonal: "))
+result = (diagonal1 * diagonal2) / 2
+print(f"result: {result}")
+
+#3
+number = int(input("Enter 4-digital number: "))
+n1 = number // 1000
+n2 = number // 100 % 10
+n3 = number // 10 % 10
+n4 = number % 10
+result = n1 * n2 * n3 * n4
+print(f"n1: {n1} n2: {n2} n3: {n3} n4: {n4}")
+print(f"result: {result}")
+
+print("hello world")
