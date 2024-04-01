cdf5a6bbac26311acaae47ee57c489182f339e90
klsss150367 <dilatan8@ukr.net>
2024-04-01 23:30:39 +0300

    second task

--- hw2/hw2.py
+++ hw2/hw2.py
@@ -9,7 +9,7 @@ print(n1 != n2)
 print(1 == 1 and 3 == 3)
 print(1 == 1 and 2 == 3)
 
-hours = int(input("Enter Hours: "))
+#hours = int(input("Enter Hours: "))
 #if hours >= 12:
     #print("PM")
     #print("kggy")
@@ -17,9 +17,20 @@ hours = int(input("Enter Hours: "))
     #print("AM")
 
     #2
-if 12 <= hours < 24:
-    print("PM")
-elif 0 <= hours < 12:
-    print("AM")
+#if 12 <= hours < 24:
+    #print("PM")
+#elif 0 <= hours < 12:
+    #print("AM")
+#else:
+    #print("Incorrect hours!")
+film_rating = int(input("Enter Film Rating: "))
+if film_rating >0 and film_rating <= 5:
+    if film_rating == 4 or film_rating == 5:
+        print("Ok")
+    else:
+        print("Not Ok")
 else:
-    print("Incorrect hours!")
+    print("incorrect rating")
+
+
+
